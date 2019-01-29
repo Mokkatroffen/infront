494c3a620a2ca1eef81a5f875d3662e65a47a897
Kristian Munter Simonsen <Mokkatroffen@gmail.com>
2019-01-29 15:14:45 +0100

    Updates

---
+++ precache-manifest.04d38c5809869bfc595ed988541e8e85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41f5e43696d2373df9c9",
+    "url": "/infront/static/js/runtime~main.41f5e436.js"
+  },
+  {
+    "revision": "6e393fd0e7cbeb1704ae",
+    "url": "/infront/static/js/main.6e393fd0.chunk.js"
+  },
+  {
+    "revision": "d8a08fb62d4ae79c90b9",
+    "url": "/infront/static/js/1.d8a08fb6.chunk.js"
+  },
+  {
+    "revision": "6e393fd0e7cbeb1704ae",
+    "url": "/infront/static/css/main.c6c7975b.chunk.css"
+  },
+  {
+    "revision": "71ebcbe570bb30e13f2f7c7f140bd1e9",
+    "url": "/infront/index.html"
+  }
+];
